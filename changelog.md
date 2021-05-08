9d85a08af4999ceb7be4909b43e3e9fae0c1e340
Andrii Nechaev <andreynech@aswsyst.cz>
2021-05-08 14:09:59 +0300

    v.0.0.4

--- changelog.md
+++ changelog.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## [0.0.4] - 2021-05-08
 
 ### Added
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='workon',
-    version='0.0.3',
+    version='0.0.4',
     author='Andrey Nechaev',
     author_email='andrewnech@gmail.com',
     packages=find_packages(exclude=('tests.*', 'tests')),
