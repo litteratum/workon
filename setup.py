62291f8a034a37e1757a89747db6a7617f9f5431
Andrii Nechaev <andreynech@aswsyst.cz>
2021-07-31 15:41:32 +0300

    fix config installation; install it in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 """setup.py for the script."""
 import os
+import shutil
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+USER_CONFIG_PATH = os.path.expanduser('~/.config/workon/config.json')
+os.makedirs(os.path.dirname(USER_CONFIG_PATH), exist_ok=True)
+
+
+class LocalInstall(install):
+
+    def run(self):
+        if not os.path.exists(USER_CONFIG_PATH):
+            shutil.copy('config.json', USER_CONFIG_PATH)
+        install.run(self)
+
 
 setup(
     name='workon',
@@ -11,7 +26,7 @@ setup(
     entry_points={
         'console_scripts': ['workon = workon.main:main']
     },
-    data_files=[
-        (os.path.expanduser('~/.config/workon/'), ['config.json']),
-    ]
+    cmdclass={
+        'install': LocalInstall,
+    },
 )
