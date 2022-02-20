21c5b18b81a865ccece7fb6627c01640e711cc98
Andrii Nechaev <andreynech@aswsyst.cz>
2022-02-20 14:14:12 +0200

    simplify git module (decomposition)

--- git_workon/git.py
+++ git_workon/git.py
@@ -7,16 +7,20 @@ class GITError(Exception):
     """Any error related with GIT usage."""
 
 
+def _run_command(
+    command: str, check=False, cwd: str = None
+) -> subprocess.CompletedProcess:
+    """Run command in subprocess."""
+    logging.debug('Running command "%s"', command)
+    return subprocess.run(
+        command.split(), cwd=cwd, capture_output=True, text=True, check=check
+    )
+
+
 def get_stash_info(directory: str):
     """Return stash info under `directory`."""
-    logging.debug('Checking GIT stashes under "%s"', directory)
-    return subprocess.run(
-        "git stash list".split(),
-        cwd=directory,
-        capture_output=True,
-        text=True,
-        check=False,
-    ).stdout
+    logging.info('Checking GIT stashes under "%s"', directory)
+    return _run_command("git stash list", cwd=directory).stdout
 
 
 def get_unpushed_branches_info(directory: str) -> str:
@@ -24,26 +28,16 @@ def get_unpushed_branches_info(directory: str) -> str:
 
     Format is: <commit> (<branch>) <commit_message>
     """
-    logging.debug('Checking for unpushed GIT commits under "%s"', directory)
-    return subprocess.run(
-        "git log --branches --not --remotes --decorate --oneline".split(),
-        cwd=directory,
-        capture_output=True,
-        text=True,
-        check=False,
+    logging.info('Checking for unpushed GIT commits under "%s"', directory)
+    return _run_command(
+        "git log --branches --not --remotes --decorate --oneline", cwd=directory
     ).stdout
 
 
 def get_unstaged_info(directory: str) -> str:
     """Return information about unstaged changes."""
-    logging.debug('Checking for unstaged changes under "%s"', directory)
-    return subprocess.run(
-        "git status --short".split(),
-        cwd=directory,
-        capture_output=True,
-        text=True,
-        check=False,
-    ).stdout
+    logging.info('Checking for unstaged changes under "%s"', directory)
+    return _run_command("git status --short", cwd=directory).stdout
 
 
 def get_unpushed_tags(directory: str) -> str:
@@ -53,15 +47,11 @@ def get_unpushed_tags(directory: str) -> str:
     If failed to get tags information, returns a string containing error
     description.
     """
-    logging.debug('Checking for unpushed tags under "%s"', directory)
+    logging.info('Checking for unpushed tags under "%s"', directory)
 
     try:
-        info = subprocess.run(
-            "git push --tags --dry-run".split(),
-            cwd=directory,
-            capture_output=True,
-            text=True,
-            check=True,
+        info = _run_command(
+            "git push --tags --dry-run", cwd=directory, check=True
         ).stderr
     except subprocess.CalledProcessError as exc:
         return f"Failed to check unpushed tags: {exc.stderr}"
@@ -74,12 +64,7 @@ def get_unpushed_tags(directory: str) -> str:
 def clone(source: str, destination: str):
     """Clone a project from GIT `source` to `destination` directory."""
     try:
-        logging.debug('Cloning "%s" to "%s"', source, destination)
-        subprocess.run(
-            ["git", "clone", source, destination],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
+        logging.info('Cloning "%s" to "%s"', source, destination)
+        _run_command(f"git clone {source} {destination}", check=True)
     except subprocess.CalledProcessError as exc:
         raise GITError(f'Failed to clone "{source}":\n{exc.stderr}') from exc
